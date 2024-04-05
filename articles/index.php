159e4181782b093b815afad11b18589f0aabd81a
eggnogg <juanpaologillera@gmail.com>
2024-04-06 03:33:47 +0800

    remove duplicate font connection

---
+++ articles/index.php
@@ -0,0 +1,27 @@
+<?php include '../php/top_of_page.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/article-list.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+
+    <title>Misinformation</title>
+</head>
+<?php include '../php/analytics.php';?>
+<body>
+    <header>
+        <?php include '../php/view_components/navbar.php';?>
+    </header>
+    <main>
+        <large-banner></large-banner>
+    </main>
+    
+</body>
+</html>
--- misinformation/index.php
+++ misinformation/index.php
@@ -8,8 +8,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/common.css">
     <link rel="stylesheet" href="../css/articles.css">
