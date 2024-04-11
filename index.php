ead80782187e8bb4534ea0385495affe6761ce3c
eggnogg <juanpaologillera@gmail.com>
2024-04-11 17:06:44 +0800

    fix links on homepage

--- index.php
+++ index.php
@@ -34,13 +34,13 @@
                 <img class="devoidLogoMAIN" src="../iteration2/images/devoidLogo.svg" alt="devoidLogo">
                 <p>Witness a young girl's journey as she navigates through the divides in the parasocial space, fueled by election-related misinformation in an emulation of the digital reality.</p>
                 <div class="main-buttons">
-                    <a href="novel/act1">
+                    <a href="summary">
                         <button>summary</button>
                     </a>
                     <a href="novel/act1">
                         <button>Read Novel</button>
                     </a>
-                    <a href="novel/act1">
+                    <a href="characters">
                         <button>characters</button>
                     </a>
                 </div>
